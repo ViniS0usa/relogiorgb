f514bca1b967ea4069ad129eaeb98090acd27320
Vini Sousa <vini.sousa2306@gmail.com>
2022-09-19 04:12:00 -0300

    começo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Relógio Digital RGB
+>Projeto de um relógio digital rgb
---
+++ js/index.js
@@ -0,0 +1,19 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dataHoje = new Date();
+    let hr = dataHoje.getHours();
+    let min = dataHoje.getMinutes();
+    let sec = dataHoje.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+
+})
